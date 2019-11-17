fdd051d459f25a15734fbf429986b03250de8041
daniloxxv <danilosoares@gmail.com>
2019-11-17 21:01:20 +0000

    Fixing saga logic

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,10 +1,7 @@
 export default function cart(state=[], action){
     switch (action.type) {
         case '@cart/ADD_SUCCESS':
-            const index = state.findIndex(el=>el.id === action.product.id)
-            return index === -1 ? 
-                [...state, {...action.product, amount: 1}] : 
-                [...state.slice(0,index), {...action.product, amount: state[index].amount + 1}, ...state.slice(index+1)]
+            return [...state, action.product]
         case '@cart/REMOVE':
             const deleteIndex = state.findIndex(el=>el.id === action.id)
             return deleteIndex === -1 ? state : [...state.slice(0,deleteIndex), ...state.slice(deleteIndex+1)]
